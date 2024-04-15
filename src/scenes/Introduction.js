d7cbcde45006395182dfae746cb430bbf2568f23
anaiysaa <anaiysaadeswal@outlook.com>
2024-04-15 07:49:58 -0700

    Spawn in scene fixed

--- src/scenes/Introduction.js
+++ src/scenes/Introduction.js
@@ -20,8 +20,8 @@ export default class Introduction extends Phaser.Scene {
     scoreText = this.add.text(10, 16, '\n  Welcome to a world where dogs reign...\n               Doglandia \n     After the human population fell at the hands of the dogs, \n    But as you explore this cabin \n         in the woods, be alert, \n  for something is amiss in the forest.', { fontSize: '32px', fill: '#000' });
 
     this.input.once('pointerdown', function() {
-      console.log('From Introduction to House');
-      this.scene.start('House');
+      console.log('From Introduction to SpawnIn');
+      this.scene.start('SpawnIn');
     }, this);
   }
 
